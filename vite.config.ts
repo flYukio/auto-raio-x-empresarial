677aab9dcf16977fa131ff5797039e17b2d97ded
flYukio <fabio.yukio.lima@gmail.com>
2026-04-07 07:56:03 -0300

    Fix: explicitly set outDir to dist for Hostinger

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig(({mode}) => {
       hmr: process.env.DISABLE_HMR !== 'true',
     },
     build: {
+      outDir: 'dist',
       rollupOptions: {
         output: {
           manualChunks(id) {
